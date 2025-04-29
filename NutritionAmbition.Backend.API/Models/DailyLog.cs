c0a2a1627fa304d80252aa2e03134a97f3ece487
shane <shane@jacobi.solutions>
2025-04-29 14:51:23 -0500

    fix

--- NutritionAmbition.Backend.API/Models/DailyLog.cs
+++ NutritionAmbition.Backend.API/Models/DailyLog.cs
@@ -10,9 +10,9 @@ namespace NutritionAmbition.Backend.API.Models
         public DateTime LoggedDateUtc { get; set; } = DateTime.UtcNow.Date;
         public List<FoodEntry> Entries { get; set; } = new List<FoodEntry>();
 
-        public double TotalCalories => Entries.Sum(entry => entry.ParsedItems.Sum(item => item.Calories));
-        public double TotalProtein => Entries.Sum(entry => entry.ParsedItems.Sum(item => item.Protein));
-        public double TotalCarbs => Entries.Sum(entry => entry.ParsedItems.Sum(item => item.Carbohydrates));
-        public double TotalFat => Entries.Sum(entry => entry.ParsedItems.Sum(item => item.Fat));
+        public double TotalCalories => Entries.Sum(entry => entry.GroupedItems.Sum(item => item.Items.Sum(i => i.Calories)));
+        public double TotalProtein => Entries.Sum(entry => entry.GroupedItems.Sum(item => item.Items.Sum(i => i.Protein)));
+        public double TotalCarbs => Entries.Sum(entry => entry.GroupedItems.Sum(item => item.Items.Sum(i => i.Carbohydrates)));
+        public double TotalFat => Entries.Sum(entry => entry.GroupedItems.Sum(item => item.Items.Sum(i => i.Fat)));
     }
 } 
